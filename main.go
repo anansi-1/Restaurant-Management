d6e0ab20ec8b1043afa4818c819b35b8ca2bd0af
anansi-1 <anansisime7@gmail.com>
2025-08-10 22:52:34 +0300

    route and collection setup

--- main.go
+++ main.go
@@ -1,1 +1,36 @@
 package main
+
+import (
+	"os"
+	"restaurant-management/database"
+	"restaurant-management/routes"
+	"restaurant-management/middleware"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"github.com/gin-gonic/gin"
+)
+
+var foodCollection *mongo.Collection = database.OpenCollection(database.Client,"food")
+
+func main() {
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8080"
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+	router.Use(middleware.Authentication())
+
+	routes.FoodRoutes(router)
+	routes.MenuRoutes(router)
+	routes.TableRoutes(router)
+	routes.OrderRoutes(router)
+	routes.OrderItemRoutes(router)
+	routes.InvoiceRoutes(router)
+
+	router.Run(":" + port)
+
+}
