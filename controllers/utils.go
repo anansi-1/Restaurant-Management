4b064c270aca4e891bedc37c470f88fcacb273fb
anansi-1 <anansisime7@gmail.com>
2025-08-13 13:57:28 +0300

    add utils

--- controllers/utils.go
+++ controllers/utils.go
@@ -1,9 +1,22 @@
 package controllers
 
-// func round (num float64) int {
+import (
+	"math"
+	"time"
+)
 
-// }
+
+func inTimeSpan(start, end, check time.Time) bool {
+	return start.After(time.Now()) && end.After(start)
+}
+
+
+func round (num float64) int {
+	return int(num + math.Copysign(0.5,num))
+}
 
 func toFixed(num float64, precision int) float64{
-	
+	output := math.Pow(10, float64(precision))
+
+	return  float64(round(num*output)) / output
 }
