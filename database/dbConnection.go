ece7ab83e85865e26e7fc5910588e4428124efdf
anansi-1 <anansisime7@gmail.com>
2025-08-15 13:47:04 +0300

    update dbConnection

--- database/dbConnection.go
+++ database/dbConnection.go
@@ -6,11 +6,17 @@ import (
 	"os"
 	"time"
 
+	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func DBinstance() *mongo.Client {
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
 
 	uri := os.Getenv("MONGODB_URI")
 
