5ca6b5d6d8a9e587d96774ac651f45d39d40cc17
anansi-1 <anansisime7@gmail.com>
2025-08-15 13:48:08 +0300

    dependencies

--- main.go
+++ main.go
@@ -3,16 +3,12 @@ package main
 import (
 	"log"
 	"os"
-	"restaurant-management/database"
 	"restaurant-management/middleware"
 	"restaurant-management/routes"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
-	"go.mongodb.org/mongo-driver/mongo"
 )
-
-var foodCollection *mongo.Collection = database.OpenCollection(database.Client,"food")
 
 func main() {
 
@@ -26,13 +22,12 @@ func main() {
 	if port == "" {
 		port = "8080"
 	}
-	
-	
+
 	router := gin.New()
 	router.Use(gin.Logger())
 
 	routes.UserRoutes(router)
-	
+
 	router.Use(middleware.Authentication())
 
 	routes.FoodRoutes(router)
