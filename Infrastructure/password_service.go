89257b47941765501f5a19d70fec5b337f21302b
anansi-1 <anansisime7@gmail.com>
2025-08-15 13:45:56 +0300

    add password service and authmiddleware

--- Infrastructure/password_service.go
+++ Infrastructure/password_service.go
@@ -1,10 +1,21 @@
 package infrastructure
 
+import "golang.org/x/crypto/bcrypt"
 
-func HashPassword(password string)(string,error){
+func HashPassword(password string) (string, error) {
 
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+
+	return string(bytes), nil
 }
 
-func VerifyPassword(userPassword,providedPassword string){
+func VerifyPassword(providedPassword, userPassword string) (bool) {
+
+	err := bcrypt.CompareHashAndPassword([]byte(userPassword), []byte(providedPassword))
+
+	return err == nil
 
 }
