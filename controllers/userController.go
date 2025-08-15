f66236b508aab51dbb080de0b3cf7a1c764f537b
anansi-1 <anansisime7@gmail.com>
2025-08-15 13:45:03 +0300

    add userController

--- controllers/userController.go
+++ controllers/userController.go
@@ -1,39 +1,204 @@
 package controllers
 
-
-// incomingRoutes.GET("/users", controller.GetUsers())
-// incomingRoutes.GET("/users/:user_id", controller.GetUser())
 // incomingRoutes.POST("/users/signup", controller.SignUp())
 // incomingRoutes.POST("/users/login", controller.Login())
 
+import (
+	"context"
+	"log"
+	"net/http"
+	infrastructure "restaurant-management/Infrastructure"
+	"restaurant-management/database"
+	"restaurant-management/helpers"
+	"restaurant-management/models"
+	"strconv"
+	"time"
 
-import(
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
+var userCollection *mongo.Collection = database.OpenCollection(database.Client, "user")
 
-func GetUsers() gin.HandlerFunc{
+// GET /users
+func GetUsers() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
 
+		recordPerPage, err := strconv.Atoi(c.Query("recordPerPage"))
+		if err != nil || recordPerPage < 1 {
+			recordPerPage = 10
+		}
+
+		page, err1 := strconv.Atoi(c.Query("page"))
+		if err1 != nil || page < 1 {
+			page = 1
+		}
+
+		startIndex := (page - 1) * recordPerPage
+		if q := c.Query("startIndex"); q != "" {
+			if si, err := strconv.Atoi(q); err == nil && si >= 0 {
+				startIndex = si
+			}
+		}
+
+		matchStage := bson.D{{Key: "$match", Value: bson.D{{}}}}
+		projectStage := bson.D{
+			{Key: "$project", Value: bson.D{
+				{Key: "_id", Value: 0},
+				{Key: "total_count", Value: 1},
+				{Key: "user_items", Value: bson.D{{Key: "$slice", Value: []interface{}{"$data", startIndex, recordPerPage}}}},
+			}}}
+
+		result, err := userCollection.Aggregate(ctx, mongo.Pipeline{
+			matchStage, projectStage})
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while listing user items"})
+		}
+
+		var allUsers []bson.M
+		if err = result.All(ctx, &allUsers); err != nil {
+			log.Fatal(err)
+		}
+		c.JSON(http.StatusOK, allUsers[0])
+
+	}
+}
+
+// GET /users/:user_id
+func GetUser() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		userId := c.Param("user_id")
+
+		var user models.User
+
+		err := userCollection.FindOne(ctx, bson.M{"user_id": userId}).Decode(&user)
+
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while listing user items"})
+		}
+		c.JSON(http.StatusOK, user)
+	}
+}
+
+// POST /users/signup
+func SignUp() gin.HandlerFunc {
 	return func(c *gin.Context) {
 
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+		var user models.User
+
+		if err := c.BindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": "Invalid request body",
+			})
+			return
+		}
+
+		validationErr := validate.Struct(user)
+		if validationErr != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": validationErr.Error()})
+			return
+		}
+
+		count, err := userCollection.CountDocuments(ctx, bson.M{"email": user.Email})
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": "error occured while checking for email",
+			})
+			return
+		}
+		if count > 0 {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "this email or phone number already exsits"})
+			return
+		}
+		password, err := infrastructure.HashPassword(*user.Password)
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": "error occured while hasing password",
+			})
+			return
+		}
+		user.Password = &password
+
+		count, err = userCollection.CountDocuments(ctx, bson.M{"phone": user.Phone})
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": "error occured while checking for phone number",
+			})
+			return
+		}
+
+		if count > 0 {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "this email or phone number already exsits"})
+			return
+		}
+
+		user.ID = primitive.NewObjectID()
+		user.CreatedAt = time.Now().UTC()
+		user.UpdatedAt = time.Now().UTC()
+		user.UserID = user.ID.Hex()
+
+		token, refreshToken, _ := helpers.GenerateAllTokens(*user.Email, *user.FirstName, *user.LastName, user.UserID)
+		user.Token = &token
+		user.RefreshToken = &refreshToken
+		resultInsertionNumber, insertErr := userCollection.InsertOne(ctx, user)
+		if insertErr != nil {
+			msg := "User item was not created"
+			c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+			return
+		}
+
+		c.JSON(http.StatusOK, resultInsertionNumber)
 	}
-	
+
 }
 
-func GetUser() gin.HandlerFunc {
+// POST /users/login
+func Login() gin.HandlerFunc {
+	return func(c *gin.Context) {
 
-	return  func(ctx *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+		var user models.User
+		var foundUser models.User
+
+		if err := c.BindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": "Invalid request body",
+			})
+			return
+		}
+
+		err := userCollection.FindOne(ctx, bson.M{"email": user.Email}).Decode(&foundUser)
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "incorrect email or password"})
+			return
+		}
+
+		passwordIsValid := infrastructure.VerifyPassword(*user.Password, *foundUser.Password)
+		defer cancel()
+		if !passwordIsValid {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+			return
+		}
+
+		// token, refreshToken, _ := helpers.GenerateAllTokens(*user.Email, *user.FirstName, *user.LastName, user.UserID)
+		// helpers.UpdateAllTokens(token, refreshToken, foundUser.UserID)
+
+		c.JSON(http.StatusOK, foundUser)
 
 	}
 }
-
-func SignUp() gin.HandlerFunc {
-	return func(ctx *gin.Context) {
-
-	}
-}
-
-func Login() gin.HandlerFunc{
-	return func(ctx *gin.Context) {}
-}
-
